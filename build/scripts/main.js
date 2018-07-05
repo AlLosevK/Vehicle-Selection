3b73aa8847554576fa7e6ec8ab4a471b6c5402c0
AlLosevK <al.losev.k@gmail.com>
2018-07-05 18:40:30 +0300

    add script for menu

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,1 +1,19 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{IhKj:function(n,s,o){"use strict";o.r(s);o("WMMs")},WMMs:function(n,s,o){}},[["IhKj",0]]]);
+(window.webpackJsonp = window.webpackJsonp || []).push([[1], {
+  IhKj: function (n, s, o) {
+    "use strict";
+    o.r(s);
+    o("WMMs")
+  },
+  WMMs: function (n, s, o) {}
+}, [["IhKj", 0]]]);
+
+$(".a__menu").click(function (e) {
+  e.preventDefault();
+  if ($(this).hasClass('a__menu-open')) {
+    $(".sidebar").fadeOut(400);
+    $(this).removeClass('a__menu-open');
+  } else {
+    $(this).addClass('a__menu-open');
+    $(".sidebar").fadeIn(400);
+  };
+})
